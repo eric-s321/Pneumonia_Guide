d2f37e9941ac7843b392b2ba8b28017a852eafa1
Eric Scagnelli <ericscagnelli@gmail.com>
2017-03-28 01:38:22 -0400

    Finished all resizing except the 2 view controllers with web links. Added in custom WebViewController

---
+++ PreOpEval2/WebViewController.m
@@ -0,0 +1,15 @@
+//
+//  WebViewController.m
+//  Pneumonia
+//
+//  Created by Eric Scagnelli on 3/28/17.
+//  Copyright © 2017 kangh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WebViewController.h"
+
+@implementation WebViewController
+
+
+@end
